0f0d5f33401c58b7004c633a0ee6553d7ad58fa4
James Hughes <jameshughes2009@hotmail.com>
2024-01-20 15:43:06 -0500

    adding request bars to live file

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var options = ["A", "B", "c"]
+
 // Write password to the #password input
+function generatePassword (){
+  var passwordLength = prompt("Please Choose Length");
+  var speicalCharacters = prompt("Do you want to add special characters")
+  var uppercase = prompt("do you want uppercase letters?")
+  console.log (passwordLength)
+  console.log (speicalCharacters, uppercase)
+}
 function writePassword() {
-  var password = function () {
-    generatePassword();
-  }
+  var password = generatePassword();
+  password = function(){
+    "ashdgahsgdajgfajs"
+  }, "asasdasdasda"
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
+console.log ("test")
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 console.log ("James Assignment")
-console.log ("passwordText")
