7a66cc36caab787dc6de7e677192560000189f5d
James Hughes <jameshughes2009@hotmail.com>
2024-01-21 10:33:41 -0500

    adding if statements and testing functionailty

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+const length = 12;
 
 // Write password to the #password input
 function generatePassword (){
   var passwordLength = prompt("Please choose a length of at least  8")
+  if (!passwordLength < 0) {
+    return;
+  }
   var speicalCharacters = prompt("Do you want to add special characters")
   var uppercase = prompt("do you want uppercase letters232?")
   console.log (passwordLength)
